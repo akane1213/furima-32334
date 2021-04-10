11c172a2fddadf9cfcc58f8e2728231a49c50761
akane1213 <76610860+akane1213@users.noreply.github.com>
2021-04-10 21:15:40 +0900

    モデルの単体テストコードの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,11 +6,15 @@ class User < ApplicationRecord
   
   with_options presence: true do
     validates :name
-    validates :first_name
-    validates :last_name
-    validates :first_name_kana
-    validates :last_name_kana
     validates :birthday
+    with_options format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
+      validates :first_name
+      validates :last_name
+    end
+    with_options format: { with: /\A[ァ-ヶ一-]+\z/, message: '全角カナを使用してください' } do
+      validates :first_name_kana
+      validates :last_name_kana
+    end
   end
 
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
