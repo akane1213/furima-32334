a7e412fbc4019e5e821d5ff206e5713b043746e7
akane1213 <76610860+akane1213@users.noreply.github.com>
2021-04-12 22:06:16 +0900

    出品した商品の情報をテーブルに保存できるようにする

--- app/controllers/furimas_controller.rb
+++ app/controllers/furimas_controller.rb
@@ -10,6 +10,17 @@ class FurimasController < ApplicationController
   end
 
   def create
-    @furima = Item.new
+    @furima = Item.new(item_params)
+    if @furima.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:product_name, :product_description, :category_id, :product_condition_id, :shipping_charges_id, :shipping_area_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,10 +10,11 @@ class Item < ApplicationRecord
   belongs_to :days_to_ship
 
   validates :product_name, presence: true
-
+  validates :product_description, presence: true
   validates :category_id, numericality: { other_than: 1 }
   validates :product_condition_id, numericality: { other_than: 1 }
   validates :shipping_charges_id, numericality: { other_than: 1 }
   validates :shipping_area_id, numericality: { other_than: 1 }
   validates :days_to_ship_id, numericality: { other_than: 1 }
+  validates :price, presence: true
 end
